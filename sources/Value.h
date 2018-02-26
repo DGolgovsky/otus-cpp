5903c3a53b86aecf5b856cfdf1edeae25c058976
Dmitry Golgovsky <d.westcoast@aol.com>
2018-02-26 13:05:39 +0300

    Small refactor

--- sources/Value.h
+++ sources/Value.h
@@ -1,13 +1,16 @@
 #pragma once
 
-template<typename T, int default_value>
-class matrix;
-
 template<typename T, int def_value = 0>
-class value
+class Value
 {
+private:
+    Matrix<T, def_value> &m_matrix;
+    T m_value = def_value;
+    int m_row{};
+    int m_col{};
+
 public:
-    value(matrix<T, def_value> &matrix_, int val_)
+    explicit Value(Matrix<T, def_value> &matrix_, int val_)
             : m_matrix(matrix_)
             , m_value(val_)
     { }
@@ -17,22 +20,25 @@ public:
         m_col = col_;
     }
 
-    void operator=(const int& val) {
-        coord_t coord = std::make_pair(m_row, m_col);
+    auto operator=(int const &val) {
+        place_t coord = std::make_pair(m_row, m_col);
+
         if (val == def_value) {
             auto it = m_matrix.get_points().find(coord);
             if (it != m_matrix.get_points().end())
                 m_matrix.get_points().erase(it);
         } else {
-            m_matrix.get_points().insert(std::make_pair(coord, value(m_matrix, val)));
+            m_matrix.get_points().insert(std::make_pair(coord, Value(m_matrix, val)));
         }
+
+        return nullptr;
     }
 
-    friend bool operator==(value<T, def_value> &lhs, T const &rhs) {
+    friend bool operator==(Value<T, def_value> &lhs, T const &rhs) {
         return lhs.get_value() == rhs;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, value<T, def_value> const &obj) {
+    friend std::ostream &operator<<(std::ostream &os, Value<T, def_value> const &obj) {
         if (obj.m_value == def_value)
             os << def_value;
         else
@@ -43,10 +49,4 @@ public:
     T get_value() const {
         return m_value;
     }
-
-private:
-    matrix<T, def_value> &m_matrix;
-    T m_value = def_value;
-    int m_row{};
-    int m_col{};
 };
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 #include <cassert>
-#include "matrix.h"
+#include "Matrix.h"
 
 int main(int, char const **)
 {
     int const def_value = 0;
     int const matrix_size = 10;
 
-    matrix<int, def_value> matr;
-    assert(matr.size() == 0);
+    Matrix<int, def_value> matrix;
 
-    auto a = matr[0][0];
-    assert(a == def_value);
-    assert(matr.size() == 0);
+    { /* Task's description checks */
+        assert(matrix.size() == 0);
 
-    matr[100][100] = 314;
-    assert(matr[100][100] == 314);
-    assert(matr.size() == 1);
+        auto a = matrix[0][0];
+        assert(a == def_value);
+        assert(matrix.size() == 0);
 
-    matr[100][100] = def_value;
-    assert(matr[100][100] == def_value);
-    assert(matr.size() == 0);
+        matrix[100][100] = 314;
+        assert(matrix[100][100] == 314);
+        assert(matrix.size() == 1);
 
-    for (int i = 0; i < matrix_size; ++i) {
-        for (int j = 0; j < matrix_size; ++j) {
-            if ((i == j) || ((i + j) == (matrix_size - 1)))
-                matr[i][j] = j;
-        }
+        matrix[100][100] = def_value;
+        assert(matrix[100][100] == def_value);
+        assert(matrix.size() == 0);
     }
 
-    for (int i = 1; i < matrix_size-1; ++i) {
-        for (int j = 1; j < matrix_size-1; ++j) {
-            std::cout << matr[i][j] << " ";
+    int matrix_index = matrix_size - 1;
+
+    for (int i = 0; i < matrix_size; ++i) {
+        matrix[i][i] = i;
+        matrix[i][matrix_index - i] = matrix_index - i;
+    }
+
+    for (int i = 1; i < matrix_index; ++i) {
+        for (int j = 1; j < matrix_index; ++j) {
+            std::cout << matrix[i][j] << " ";
         }
         std::cout << std::endl;
     }
 
-    std::cout << matr.size() << std::endl;
+    std::cout << matrix.size() << std::endl;
 
     int x = 0;
     int y = 0;
     int v = 0;
-    for(auto c : matr) {
-        std::tie(x, y, v) = matrix<int, def_value>::make_tuple(c);
+    for(auto const &cell : matrix) {
+        std::tie(x, y, v) = Matrix<int, def_value>::make_tuple(cell);
         std::cout << "[" << x << "," << y << "]" << " = " << v << std::endl;
     }
 
